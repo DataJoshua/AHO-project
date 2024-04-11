58da298ff3d9d03309ccd3cf2588d2a38107cdea
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-04-11 20:55:11 +0400

    admin dashboard created

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,7 +1,9 @@
 module Admin
   class DashboardsController < ApplicationController
     before_action :authenticate_user!
-    expose :under_review_posts, -> { Post.under_review.kept }
+
+    expose :posts_under_review, -> { Post.under_review.kept }
+    expose :posts, -> { current_user.posts.kept }
 
     def show; end
   end
