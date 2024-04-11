79a44c586f02ca70af03409e8988d158c1863faf
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-04-11 13:14:49 +0400

    added decent_exposure and adapted regions_controller

--- Gemfile
+++ Gemfile
@@ -79,3 +79,5 @@ group :test do
 end
 
 gem "rolify", "~> 6.0"
+
+gem "decent_exposure", "~> 3.0"
--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,3 +1,8 @@
 class RegionsController < ApplicationController
+  expose :regions, -> { Region.all }
+  expose :region
+
   def index; end
+
+  def show; end
 end
