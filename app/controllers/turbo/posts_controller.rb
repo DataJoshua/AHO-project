fefee10ce1d9587210e5d3d247d95cc3822f509e
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-04-12 10:01:27 +0400

    added null selection to show all posts

--- app/controllers/turbo/posts_controller.rb
+++ app/controllers/turbo/posts_controller.rb
@@ -2,7 +2,7 @@ module Turbo
   class PostsController < ApplicationController
     include Rails.application.routes.url_helpers
 
-    expose :raw_posts, -> { Post.approved }
+    expose :raw_posts, -> { Post.approved.kept }
     expose :posts, -> { PostQuery.new(query_params, raw_posts).all }
 
     def index; end
