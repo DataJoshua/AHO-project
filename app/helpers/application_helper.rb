71360c69257e31a1cc02295a6c7cbf1f4260ce7e
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-04-12 17:09:06 +0400

    styles finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,21 @@
 module ApplicationHelper
   def header_link_class
-    "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+    "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
+  end
+
+  def btn_class
+    "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none"
+  end
+
+  def btn_red_class
+    "text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none"
+  end
+
+  def btn_green_class
+    "text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none"
+  end
+
+  def state_class
+    "absolute rounded-lg bg-blue-100 py-1 px-2 border border-blue-500 text-sm top-[-5%] right-[-5%] z-100"
   end
 end
---
+++ app/helpers/dashboard_helper.rb
@@ -0,0 +1,5 @@
+module DashboardHelper
+  def dashboard_title_class
+    "mb-2 my-4 text-center text-2xl font-bold tracking-tight text-gray-900"
+  end
+end
