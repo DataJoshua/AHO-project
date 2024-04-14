d55f50d7d788d4dcc55b6a4cba44c85d8dd38714
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-04-15 01:20:10 +0400

    added breadcrumbs

---
+++ app/helpers/breadcrumbs_helper.rb
@@ -0,0 +1,5 @@
+module BreadcrumbsHelper
+  def breadcrumb_link
+    "text-primary transition duration-150 ease-in-out hover:text-primary-accent-300 focus:text-primary-accent-300 active:text-primary-accent-300 motion-reduce:transition-none dark:text-primary-400"
+  end
+end
