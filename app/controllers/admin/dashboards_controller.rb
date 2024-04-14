12078635904c7a3037a9d794911290acfcd7dce5
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-04-15 00:59:04 +0400

    pagination in admin panel

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,7 +3,10 @@ module Admin
     before_action :authenticate_user!
 
     expose :posts_under_review, -> { Post.under_review.includes(:user).kept }
-    expose :posts, -> { current_user.posts.kept }
+
+    expose :raw_posts, -> { current_user.posts.kept }
+    expose :pagy_posts, -> { pagy(raw_posts, items: 6) }
+    expose :posts, -> { pagy_posts.last }
 
     def show; end
   end
